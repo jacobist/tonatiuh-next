f3b4cbbce0d44d8a26df3bf85de01e5528d6b430
jacobist <jacobslegorreta@gmail.com>
2022-12-23 08:00:21 -0800

    adds footer to pages

---
+++ components/footer/footer.js
@@ -0,0 +1,27 @@
+import Image from 'next/image'
+import style from './footer.module.css'
+
+export default function Footer() {
+  return (
+    <footer className={style.footer}>
+      <div className={style.left}>
+        <a href="mailto:hermes@email.com">hermes@email.com</a>
+        <a href="tel:+1">(888)888-8888</a>
+      </div>
+      <Image
+        src="/vercel.svg"
+        alt="Vercel Logo"
+        className={style.vercelLogo}
+        width={100}
+        height={24}
+        priority
+      />
+      <div className={style.right}>
+        <p>
+          © Hermes Tonatiuh Photography & Design.{" "}
+          <span>All Rights Reserved</span>
+        </p>
+      </div>
+    </footer>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import '../styles/globals.css'
 import Navbar from "../components/navbar/navBar";
+import Footer from '../components/footer/footer';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Navbar />
       <Component {...pageProps} />
+      <Footer/>
     </>
   );
 }
