76ad4163ebb411131abf84ed4751efe6e91d3a15
jacobist <jacobslegorreta@gmail.com>
2023-01-14 22:43:14 -0800

    basic input fields

--- components/form/contact.js
+++ components/form/contact.js
@@ -1,7 +1,9 @@
 import { useState, useRef } from 'react'
 import Image from 'next/image';
-import { FaFacebookF, FaInstagram } from "react-icons/fa";
 import HandleFormSubmit from './Submit'
+import style from './contact.module.css'
+import generic from "../../styles/generic.module.css";
+import { FaFacebookF, FaInstagram } from "react-icons/fa";
 
 
 export default function Contact () {
@@ -16,88 +18,103 @@ export default function Contact () {
   }
 
   return (
-    <main>
-      <div>
-        <h1>Get in touch</h1>
-        <h3>Let&apos;s create!</h3>
-        <p>Tonatiuh@gmail.com</p>
-        <Image src="/imageFiller.png" alt="filler" width="200" height="200" />
-        <a href="#">
-          <FaFacebookF />
-        </a>
-        <a href="#">
-          <FaInstagram />
-        </a>
+    <main className={style.main}>
+      <div className={style.contact}>
+        <h1 className={generic.text2XL}>Get in touch</h1>
+        <div className={style.info}>
+          <div>
+            <h3>Let&apos;s create!</h3>
+            <p>Tonatiuh@gmail.com</p>
+            <br/>
+            <p>(661) 406-1519</p>
+          </div>
+          <Image src="/imageFiller.png" alt="filler" width="250" height="250" />
+        </div>
+        <div className={style.links}>
+          <a href="#">
+            <FaFacebookF />
+          </a>
+          <a href="#">
+            <FaInstagram />
+          </a>
+        </div>
       </div>
       <form
+        className={style.form}
         ref={form}
         action=""
         name="google-sheet"
         onSubmit={handleFormSubmit}
       >
-        <div>
-          <div>
-            <label htmlFor="name">Name</label>
-            <input type="text" id="name" name="name" placeholder="name" />
-          </div>
-          <div>
-            <label htmlFor="company">Company</label>
-            <input type="text" id="company" name="company" placeholder="optional" />
-          </div>
-          <div>
-            <label htmlFor="email">Email</label>
-            <input type="email" id='email' name="email" placeholder="e-mail" />
-          </div>
-          <div>
-            <label htmlFor="phone">Phone</label>
-            <input type="tel" id='phone' name="phone" placeholder="(000)000-0000" />
-          </div>
-            <label htmlFor="message">Message</label>
+        <div className={style.input}>
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" name="name" placeholder="name" />
+        </div>
+        <div className={style.input}>
+          <label htmlFor="company">Company</label>
+          <input
+            type="text"
+            id="company"
+            name="company"
+            placeholder="optional"
+          />
+        </div>
+        <div className={style.input}>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" name="email" placeholder="e-mail" />
+        </div>
+        <div className={style.input}>
+          <label htmlFor="phone">Phone</label>
+          <input
+            type="tel"
+            id="phone"
+            name="phone"
+            placeholder="(000)000-0000"
+          />
+        </div>
+        <div className={style.input}>
+          <label htmlFor="message">Message</label>
           <textarea
-            maxLength="300"
+            maxLength="400"
             id="message"
             name="message"
             placeholder="messsage"
           />
-          <fieldset>
-            <legend>
-              <p>Services</p>
-            </legend>
-            <div>
-              <input
-                id="portraits"
-                type="checkbox"
-                name="service"
-                value="portraits"
-              />
-              <label htmlFor="portraits">Portraits</label>
-            </div>
-            <div>
-              <input
-                id="couples"
-                type="checkbox"
-                name="service"
-                value="couples"
-              />
-              <label htmlFor="couples">Couples</label>
-            </div>
-            <div>
-              <input
-                id="productPhotography"
-                type="checkbox"
-                name="service"
-                value="productPhotography"
-              />
-              <label htmlFor="productPhotography">
-                Product Photography
-              </label>
-            </div>
-          </fieldset>
-          <button >
-            {sent ? "Sent!" : "Send Message"}
-          </button>
         </div>
+        <fieldset>
+          <legend>
+            <p>Services</p>
+          </legend>
+          <div>
+            <input
+              id="portraits"
+              type="checkbox"
+              name="service"
+              value="portraits"
+            />
+            <label htmlFor="portraits">Portraits</label>
+          </div>
+          <div>
+            <input
+              id="couples"
+              type="checkbox"
+              name="service"
+              value="couples"
+            />
+            <label htmlFor="couples">Couples</label>
+          </div>
+          <div>
+            <input
+              id="productPhotography"
+              type="checkbox"
+              name="service"
+              value="productPhotography"
+              />
+            <label htmlFor="productPhotography">Product Photography</label>
+          </div>
+        </fieldset>
+        <button>{sent ? "Sent!" : "Send Message"}</button>
       </form>
     </main>
-  )
+  );
 }
