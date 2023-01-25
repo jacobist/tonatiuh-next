fca8b96e5ad543ee95e84b3fc086a70d83d80074
jacobist <jacobslegorreta@gmail.com>
2023-01-25 00:23:56 -0800

    FEATURE: adds productPhotography page prototype

--- pages/product-photography.js
+++ pages/product-photography.js
@@ -1,7 +1,5 @@
 import Image from "next/image";
-import style from "../styles/Couples.module.css";
 import generic from "../styles/generic.module.css";
-import filler from "../public/imageFiller.png";
 import fillerAlt from "../public/imageFillerVertical.png";
 
 
@@ -9,8 +7,8 @@ export default function Collection() {
   return (
     <>
       <main className={generic.main}>
-        <section className={style.section}>
-          <div className={style.gallery}>
+        <section className={generic.section}>
+          <div className={generic.gallery}>
             <Image src={fillerAlt} alt="" width={300} height={400} />
             <Image src={fillerAlt} alt="" width={300} height={400} />
             <Image src={fillerAlt} alt="" width={300} height={400} />
