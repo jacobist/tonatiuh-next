a1dae5f236ed7fdb0497560f4e65708daff656bf
jacobist <jacobslegorreta@gmail.com>
2023-02-06 14:59:57 -0800

    FEATURE: adds array cycling functionality

--- components/slideshow/slideshow.js
+++ components/slideshow/slideshow.js
@@ -1,16 +1,30 @@
 import style from './slideshow.module.css'
-import React, {useState} from 'react'
+import React, { useState, useEffect} from 'react'
 import Image from 'next/image'
 
-export default function Slideshow({slides}) {
+export default function Slideshow({album}) {
+  const [ slides, setSlides] = useState(album)
+
+  const slide = () => {
+    //creates temp array to amnipulate
+    let arr = [...slides]
+    //rearrange array
+    let item = arr.shift()
+    arr.push(item)
+    //set new array
+    setSlides(arr)
+  }
+
+
   return (
     <div className={style.slider}>
       <div className={style.show}>
         {slides.map((image, index) => (
           <div className={style.slide} key={index}>
-            <Image src={image} alt="" fill/>
+            <Image src={image} alt="" fill priority/>
           </div>
         ))}
+        <button onClick={slide}> shift </button>
       </div>
     </div>
   );
